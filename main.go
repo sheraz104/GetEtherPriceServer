0bcb06f8dff6d09344d331d529b55f27917f6f25
Sheraz Arshad <sheraz1392@gmail.com>
2019-01-11 05:11:33 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/etherPrice", func(c *gin.Context) {
+
+		price, err := getPrice()
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"ETH-USD": "$ " + price,
+		})
+	})
+
+	router.Run(":8888")
+}
+
+func getPrice() (string, error) {
+	response, err := http.Get("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken")
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	var result map[string]interface{}
+
+	json.Unmarshal([]byte(contents), &result)
+
+	price := result["result"].(map[string]interface{})["ethusd"].(string)
+
+	return price, nil
+}
